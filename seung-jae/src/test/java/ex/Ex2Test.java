d7dd13333284eece1ee48f4d5acd819d6f243e0c
lsj8367 <lsj8367@naver.com>
2021-10-24 17:57:01 +0900

    refactor: 줄맞춤, 메소드 이름 변경

--- seung-jae/src/test/java/ex/Ex2Test.java
+++ seung-jae/src/test/java/ex/Ex2Test.java
@@ -6,17 +6,16 @@ import org.junit.jupiter.api.Test;
 public class Ex2Test {
 
     @Test
-    void test() {
-       Ex2 ex2 = new Ex2(10, 20);
+    void 테스트_예제2() {
+        Ex2 ex2 = new Ex2(10, 20);
 
-       assertThat(ex2.getA()).isEqualTo(10);
-       assertThat(ex2.getB()).isEqualTo(20);
+        assertThat(ex2.getA()).isEqualTo(10);
+        assertThat(ex2.getB()).isEqualTo(20);
 
-       ex2.swap();
+        ex2.swap();
 
-       assertThat(ex2.getA()).isEqualTo(20);
-       assertThat(ex2.getB()).isEqualTo(10);
+        assertThat(ex2.getA()).isEqualTo(20);
+        assertThat(ex2.getB()).isEqualTo(10);
     }
-
 
 }
